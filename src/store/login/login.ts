7dcaadacb07970df0ff0228c656f1a5ae5b759c9
sevgilid <1209630831@qq.com>
2023-08-07 17:13:23 +0800

    完成user-content界面分页器功能，完成user界面新增/删除/查询用户功能

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -9,6 +9,7 @@ import { localCache } from '@/utils/cache'
 import { mapMenusToRoutes } from '@/utils/map-menus'
 import router from '@/router'
 import { LOGIN_TOKEN } from '@/global/constants'
+import useMainStore from '../main/main'
 
 interface ILoginState {
   token: string
@@ -40,12 +41,14 @@ const useLoginStore = defineStore('login', {
       const userMenusResult = await getUserMenusByRoleId(this.userInfo.role.id)
       const userMenus = userMenusResult.data
       this.userMenus = userMenus
-      // 测试代码
-      console.log(this.userMenus)
 
       // 4.进行本地缓存
       localCache.setCache('userInfo', userInfo)
       localCache.setCache('userMenus', userMenus)
+
+      // 5.请求所有roles/departments数据
+      const mainStore = useMainStore()
+      mainStore.fetchEntireDataAction()
 
       // 重要: 动态的添加路由
       const routes = mapMenusToRoutes(userMenus)
@@ -64,6 +67,10 @@ const useLoginStore = defineStore('login', {
         this.token = token
         this.userInfo = userInfo
         this.userMenus = userMenus
+
+        // 1..请求所有roles/departments数据
+        const mainStore = useMainStore()
+        mainStore.fetchEntireDataAction()
 
         // 2.动态添加路由
         const routes = mapMenusToRoutes(userMenus)
